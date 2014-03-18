eabd84f2c5ad14761649bb07f9ecdae14a482c2a
RJB <RJB@RJB-PC.lan>
2014-03-18 17:35:27 -0400

    Code as of 3/18/2014, 4:30 PM.

--- iterativeGitHubCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ iterativeGitHubCode/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -43,7 +43,7 @@ public class RobotTemplate extends IterativeRobot {
     boolean longFiring = false; //Is the shooter performing a long shot?
     boolean isTimed = false; //Has the shooter delay timer running out?
     double shotTime; //Pegged to the FGPA timestamp, i.e. the global CRIO system time
-    boolean flag1 = true;
+    boolean flag1 = true; //Flags for changing drive directions
     boolean flag2 = true;
 
     // Declare motor controller objects for the robot drive system
@@ -150,6 +150,8 @@ public class RobotTemplate extends IterativeRobot {
             Timer.delay(1);
             moveRollerArmUp();
         }
+        
+        //Robobees autonomous
         //start same way
         moveRollerArmDown();
         Timer.delay(1.5);
